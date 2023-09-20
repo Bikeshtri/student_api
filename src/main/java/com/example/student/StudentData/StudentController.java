c0a5547779e039c922cd932c2eae058f0ebb4210
TRICONHOME\usha.reddyr <usha.reddyr@triconinfotech.com>
2023-09-20 13:51:48 +0530

    "Swagger implementation"

--- src/main/java/com/example/student/StudentData/StudentController.java
+++ src/main/java/com/example/student/StudentData/StudentController.java
@@ -14,31 +14,31 @@ public class StudentController {
         @Autowired
         private StudentService studentService;
 
-        @RequestMapping("/students")
+        @GetMapping("/students")
         @ApiOperation("Fetches all the students")
         public List<Student> getAllTopics() {
             return studentService.getAllStudents();
         }
 
-        @RequestMapping("/students/{id}")
+        @GetMapping("/students/{id}")
         @ApiOperation("Fetches student based on ID")
         public Optional<Student> getTopic(@PathVariable String id) {
             return studentService.getStudent(id);
         }
 
-        @RequestMapping(method = RequestMethod.POST, value = "/student")
+        @PostMapping(value = "/student")
         @ApiOperation("Adds student to list")
         public void addTopic(@RequestBody Student student){
             studentService.addStudent(student);
         }
 
-        @RequestMapping(method = RequestMethod.PUT, value = "/students/{id}")
+        @PutMapping(value = "/students/{id}")
         @ApiOperation("Updates specific student details")
         public void updateTopic(@RequestBody Student student, @PathVariable String id){
             studentService.updateStudent(id, student);
         }
 
-        @RequestMapping(method = RequestMethod.DELETE, value = "/students/{id}")
+        @DeleteMapping(value = "/students/{id}")
         @ApiOperation("Deletes student from list")
         public void deleteTopic(@PathVariable String id){
             studentService.deleteStudent(id);
